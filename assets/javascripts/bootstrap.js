4b684044f886617072f9035444d06bb30a9062f0
Jean Bredeche <jean@quantopian.com>
2017-09-11 16:17:13 -0400

    Patch support for modern jquery.

--- assets/javascripts/bootstrap.js
+++ assets/javascripts/bootstrap.js
@@ -1517,7 +1517,10 @@ if (typeof jQuery === 'undefined') {
 
       var complete = function () {
         var prevHoverState = that.hoverState
-        that.$element.trigger('shown.bs.' + that.type)
+        // QUANTOPIAN CHANGED
+        if (that.$element != null) {
+          that.$element.trigger('shown.bs.' + that.type)
+        }
         that.hoverState = null
 
         if (prevHoverState == 'out') that.leave(that)
@@ -1761,7 +1764,10 @@ if (typeof jQuery === 'undefined') {
     var that = this
     clearTimeout(this.timeout)
     this.hide(function () {
-      that.$element.off('.' + that.type).removeData('bs.' + that.type)
+      // QUANTOPIAN CHANGED
+      if (that.$element != null) {
+        that.$element.off('.' + that.type).removeData('bs.' + that.type)
+      }
       if (that.$tip) {
         that.$tip.detach()
       }
